b9fad22c69b8a1184e21d8294bda1a2ada5439fc
Andrew Dolder P.E <andrew@vitaltrades.com>
2020-05-11 18:04:31 -0500

    Update commandline_string.c

--- zorro_folder/Strategy/commandline_string.c
+++ zorro_folder/Strategy/commandline_string.c
@@ -1,15 +1,16 @@
-// Note: this only works if arbitrary input is first!
-// i.e. no other quoted items are allowed unless they come later in the command line.
+// Note: this only works if arbitrary input string is in double quotations.
 // example commandline entry:
 // zorro -u "alpha beta" -run commandline_string.c
 // output: 'alpha beta'
 
-// returns a temporary string with -u output
+// returns a temporary string with -u output, if it's in double quotations.
 string get_u_output(){
 	int len = strlen(report(3));
 	string str = zalloc(len);
 	strcpy(str,report(3));
-	strtok(str,"\"");
+	char* p = strstr(str,"-u \"");
+	if(!p) return NULL;
+	if(!strtok(p,"\"")) return NULL;
 	return strtok(0,"\"");
 }
 
